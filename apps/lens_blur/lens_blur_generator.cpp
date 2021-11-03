a7420748ead3d3c53eaae6b61eac8f9dc6cd0502
Kavya Sreedhar <skavya@stanford.edu>
2021-11-03 14:18:05 -0700

    initial changes

--- apps/lens_blur/lens_blur_generator.cpp
+++ apps/lens_blur/lens_blur_generator.cpp
@@ -6,6 +6,7 @@ namespace {
 
 using namespace Halide;
 
+
 class LensBlur : public Halide::Generator<LensBlur> {
 public:
     Input<Buffer<uint8_t>>  left_im{"left_im", 3};
@@ -21,11 +22,19 @@ public:
 
     Output<Buffer<float>>   final{"final", 3};
 
+    GeneratorParam<uint8_t> fixed_point{"fixed_point", 2};
+
     void generate() {
         /* THE ALGORITHM */
-        Expr maximum_blur_radius =
-            cast<int>(max(slices - focus_depth, focus_depth) * blur_radius_scale);
 
+        // Expr maximum_blur_radius =
+        //     cast<int>(max(slices - focus_depth, focus_depth) * blur_radius_scale);
+
+        Expr blur_radius_scale_fixed = blur_radius_scale << fixed_point;
+        Expr max_slices_focus_depth = max(slices - focus_depth, focus_depth);
+        Expr maximum_blur_radius = ((max_slices_focus_depth) << fixed_point) * blur_radius_scaled_fixed;
+
+        // ask Jeff - any special handling?
         Func left = BoundaryConditions::repeat_edge(left_im);
         Func right = BoundaryConditions::repeat_edge(right_im);
 
@@ -33,60 +42,94 @@ public:
         diff(x, y, z, c) = min(absd(left(x, y, c), right(x + 2*z, y, c)),
                                absd(left(x, y, c), right(x + 2*z + 1, y, c)));
 
+        Expr diff_0 = diff(x, y, z, 0) << fixed_point;
+        Expr diff_1 = diff(x, y, z, 1) << fixed_point;
+        Expr diff_2 = diff(x, y, z, 2) << fixed_point;
+
         Func cost;
-        cost(x, y, z) = (pow(cast<float>(diff(x, y, z, 0)), 2) +
+        cost(x, y, z) = (diff_0 * diff_0) + (diff_1 * diff_1) + (diff_2 * diff_2);
+
+			 /*(pow(cast<float>(diff(x, y, z, 0)), 2) +
                          pow(cast<float>(diff(x, y, z, 1)), 2) +
-                         pow(cast<float>(diff(x, y, z, 2)), 2));
+                         pow(cast<float>(diff(x, y, z, 2)), 2));*/
 
         // Compute confidence of cost estimate at each pixel by taking the
         // variance across the stack.
         Func cost_confidence;
         {
             RDom r(0, slices);
-            Expr a = sum(pow(cost(x, y, r), 2)) / slices;
-            Expr b = pow(sum(cost(x, y, r) / slices), 2);
+
+            // ask Jeff - how do we specify slices input should be used for the LUT?
+            Func cost_by_slices_ratio;
+            Func cost_by_slices_rom_div_lookup;
+            // ask Jeff: - 32 bits?
+            cost_by_slices_rom_div_lookup(x) = u16(u32(1 << fixed_point) / u32(x));
+
+            Expr a = ((sum(cost(x, y, r) * cost(x, y, r)) << fixed_point) * cost_by_slices_rom_div_lookup) >> fixed_point; // / slices;
+            Expr c = (sum(cost(x, y, r) << fixed_point) * cost_by_slices_rom_div_lookup) >> fixed_point;
+            Expr b = (c * c) >> fixed_point;
             cost_confidence(x, y) = a - b;
         }
 
         // Do a push-pull thing to blur the cost volume with an
         // exponential-decay type thing to inpaint over regions with low
         // confidence.
+
         Func cost_pyramid_push[8];
         cost_pyramid_push[0](x, y, z, c) =
-            select(c == 0, cost(x, y, z) * cost_confidence(x, y), cost_confidence(x, y));
+            select(c == 0, ((cost(x, y, z) << fixed_point) * cost_confidence(x, y)) >> fixed_point, cost_confidence(x, y));
 
         Expr w = left_im.dim(0).extent(), h = left_im.dim(1).extent();
         for (int i = 1; i < 8; i++) {
-            cost_pyramid_push[i](x, y, z, c) = downsample(cost_pyramid_push[i-1])(x, y, z, c);
-            w /= 2;
-            h /= 2;
+
+            // ask Jeff - are upsample and downsample supported?
+            Func downsample_func;
+            downsample_func(x, y, z, c) = cost_pyramid_push[i - 1](2 * x, 2 * y, z, c);
+
+            cost_pyramid_push[i](x, y, z, c) = downsample_func(x, y, z, c); //downsample(cost_pyramid_push[i-1])(x, y, z, c);
+            w = w >> 1; // w /= 2;
+            h = h >> 1; //h /= 2;
+            // ask Jeff about boundary condition (same as above)
             cost_pyramid_push[i] = BoundaryConditions::repeat_edge(cost_pyramid_push[i], {{0, w}, {0, h}});
         }
 
         Func cost_pyramid_pull[8];
         cost_pyramid_pull[7](x, y, z, c) = cost_pyramid_push[7](x, y, z, c);
         for (int i = 6; i >= 0; i--) {
+            // ask Jeff if upsample is supported or if we should do a conv for this with RDom
+            // ask Jeff about lerp
             cost_pyramid_pull[i](x, y, z, c) = lerp(upsample(cost_pyramid_pull[i+1])(x, y, z, c),
                                                     cost_pyramid_push[i](x, y, z, c),
                                                     0.5f);
         }
 
+        // should we use the same LUT function from above instead defining a new one?
+        // ask Jeff - how do we specify cost_pyramid_pull[0](x, y, z, 1) should be used for the LUT?
+        Func filtered_cost_ratio;
+        Func filtered_cost_rom_div_lookup;
+        // ask Jeff: - 32 bits?
+        filtered_cost_rom_div_lookup(x) = u16(u32(1 << fixed_point) / u32(x));
+
         Func filtered_cost;
-        filtered_cost(x, y, z) = (cost_pyramid_pull[0](x, y, z, 0) /
-                                  cost_pyramid_pull[0](x, y, z, 1));
+        // ask Jeff - should cost pyramid pull fixed_point
+        filtered_cost(x, y, z) = cost_pyramid_pull[0](x, y, z, 0) * filtered_cost_rom_div_lookup;
+
+				  // (cost_pyramid_pull[0](x, y, z, 0) /
+                                  // cost_pyramid_pull[0](x, y, z, 1));
 
         // Assume the minimum cost slice is the correct depth.
         Func depth;
         {
             RDom r(0, slices);
+            // ask Jeff if argmin is supported
             depth(x, y) = argmin(filtered_cost(x, y, r))[0];
         }
 
         Func bokeh_radius;
-        bokeh_radius(x, y) = abs(depth(x, y) - focus_depth) * blur_radius_scale;
+        bokeh_radius(x, y) = (((absd(depth(x, y), focus_depth)) << fixed_point) * blur_radius_scale_fixed) >> fixed_point;
 
         Func bokeh_radius_squared;
-        bokeh_radius_squared(x, y) = pow(bokeh_radius(x, y), 2);
+        bokeh_radius_squared(x, y) = (bokeh_radius(x, y) * bokeh_radius(x, y)) >> fixed_point;
 
         // Take a max filter of the bokeh radius to determine the
         // worst-case bokeh radius to consider at each pixel. Makes the
@@ -100,15 +143,18 @@ public:
         }
 
         Func input_with_alpha;
-        input_with_alpha(x, y, c) = select(c == 0, cast<float>(left(x, y, 0)),
-                                           c == 1, cast<float>(left(x, y, 1)),
-                                           c == 2, cast<float>(left(x, y, 2)),
-                                           255.0f);
+        input_with_alpha(x, y, c) = select(c == 0, (left(x, y, 0) << fixed_point),
+                                           c == 1, (left(x, y, 1) << fixed_point),
+                                           c == 2, (left(x, y, 2) << fixed_point),
+                                           // ask Jeff - can we make this an integer?
+                                           255);
+                                           // 255.0f);
 
         // Render a blurred image
         Func output;
         output(x, y, c) = input_with_alpha(x, y, c);
 
+        /*
         // The sample locations are a random function of x, y, and sample
         // number (not c).
         Expr worst_radius = worst_case_bokeh_radius(x, y);
@@ -147,8 +193,16 @@ public:
         sample_x = x + sample_locations(x, y, s)[0];
         sample_y = y + sample_locations(x, y, s)[1];
         output(x, y, c) += sample_weight(x, y, s) * input_with_alpha(sample_x, sample_y, c);
+        */
 
         // Normalize
+        // should we use the same LUT function from above instead defining a new one?
+        // ask Jeff - how do we specify cost_pyramid_pull[0](x, y, z, 1) should be used for the LUT?
+        Func normalize_ratio;
+        Func normalize_rom_div_lookup;
+        // ask Jeff: - 32 bits?
+        normalize_rom_div_lookup(x) = u16(u32(1 << fixed_point) / u32(x));
+
         final(x, y, c) = output(x, y, c) / output(x, y, 3);
 
         /* THE SCHEDULE */
